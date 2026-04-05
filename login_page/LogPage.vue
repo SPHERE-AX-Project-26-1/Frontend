e66e980d2ee9df82128c04744662895b9e379802
seongee702 <seongee702@gmail.com>
2026-04-05 20:14:15 +0900

    Add files via upload

---
+++ login_page/LogPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <DefaultLayout>
+    <LogContent />
+  </DefaultLayout>
+</template>
+
+<script setup>
+import DefaultLayout from '@/components/DefaultLayout.vue'
+import LogContent from '@/components/LogPage.vue'
+</script>
